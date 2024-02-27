03ac2645e3f3c8663e92dabdf32f771a8eb8052f
Chuigda <icey@icey.tech>
2024-02-27 10:21:11 +0800

    I guess it should be like this

--- src-tauri/src/analyse.rs
+++ src-tauri/src/analyse.rs
@@ -21,7 +21,11 @@ pub async fn evaluate_position(req: EvaluateRequest) -> Result<Vec<i64>, Box<dyn
         let go_result = engine.go(job).await?;
         let score = match go_result.ai.score {
             uciengine::analysis::Score::Cp(centipawn) => centipawn,
-            uciengine::analysis::Score::Mate(_) => 999999
+            uciengine::analysis::Score::Mate(n) => if n < 0 {
+                -99999
+            } else {
+                99999
+            }
         } as i64;
 
         scores.push(score);
