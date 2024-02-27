2b53902d35e9fff2329c88bb63ecba0af9929ef1
Chuigda <icey@icey.tech>
2024-02-27 12:00:00 +0800

    Give me a plan!

--- src/index.tsx
+++ src/index.tsx
@@ -3,32 +3,66 @@ import { invoke } from '@tauri-apps/api/tauri'
 
 import { $ } from './min-jquery'
 import { EvaluateRequest, EvaluateResponse } from './protocol'
+import { Chess } from 'chess.js'
 
-async function evalPosition() {
-    const fen = ($('fen') as HTMLInputElement).value
-    const request: EvaluateRequest = {
-        positions: [fen],
-        without_king: true,
-    }
-
-    const response = await invoke('evaluate', { request }) as { success: boolean, result?: EvaluateResponse }
-    const evalResult = $('evalResult')!
-    evalResult.innerHTML = ''
-    if (response.success) {
-        evalResult.appendChild(<div>
-            <span>Score: {response.result!.scores[0]}</span>
-        </div>)
-    }
-}
+const game = new Chess()
 
 async function applicationStart() {
     $('body')!.appendChild(<div>
+        <code><pre>{ game.ascii() }</pre></code>
         <span>
-            FEN: <input id="fen" type="text"></input>
-            <button onClick={evalPosition}>Evaluate</button>
+            MOVE:
+            <input id="move" type="text"></input>
+            <button onClick={onMove}>Move</button>
         </span>
-        <div id="evalResult"></div>
+        <br />
+        <code style="color: red">
+            <pre id="debuginfo">
+            </pre>
+        </code>
     </div>)
+}
+
+async function onMove() {
+    const move = ($('move')! as HTMLInputElement).value as string
+    try {
+        game.move(move)
+    } catch (e) {
+        $('debuginfo')!.textContent = `Error: invalid move: ${e}`
+    }
+
+    const initialPosition = game.fen()
+    const positions = [initialPosition]
+
+    const candidateMoves = game.moves({ verbose: true })
+    if (candidateMoves.length === 0) {
+        $('debuginfo')!.textContent = 'Game over'
+        return
+    }
+
+    for (const move of candidateMoves) {
+        game.move(move)
+        positions.push(game.fen())
+        game.undo()
+    }
+
+    const request: EvaluateRequest = { positions, without_king: false }
+    const response = await invoke<{
+        success: boolean,
+        result: EvaluateResponse | undefined,
+    }>('evaluate', { request })
+
+    if (!response.success) {
+        $('debuginfo')!.textContent = 'Error: evaluation failed'
+        return
+    }
+
+    const scores = response.result!.scores
+    $('debuginfo')!.textContent =
+    `Initial position, white score: ${-scores[0]}\n
+Candidate moves: ${candidateMoves.map(move => move.lan).join(',')}\n
+Scores: ${scores.slice(1).join(',')}`
+
 }
 
 document.addEventListener('DOMContentLoaded', applicationStart)
