647fa35e8bd4838b15d67db50b5e5066be4f9f99
icey <icey@icey.tech>
2024-02-29 21:08:15 +0800

    Support Linux fairy-stockfish

--- src-tauri/src/analyse.rs
+++ src-tauri/src/analyse.rs
@@ -3,8 +3,14 @@ use std::{error::Error, panic::catch_unwind};
 use uciengine::uciengine::{GoJob, UciEngine};
 use crate::protocol::EvaluateRequest;
 
+#[cfg(target_os="windows")]
+const STOCKFISH_PATH: &str = "stockfish/fairy-stockfish.exe";
+
+#[cfg(target_os="linux")]
+const STOCKFISH_PATH: &str = "stockfish/fairy-stockfish";
+
 pub async fn evaluate_position(req: EvaluateRequest) -> Result<Vec<i64>, Box<dyn Error>> {
-    let engine = catch_unwind(|| UciEngine::new_with_args("stockfish/fairy-stockfish.exe", &[
+    let engine = catch_unwind(|| UciEngine::new_with_args(STOCKFISH_PATH, &[
         "load",
         "stockfish/variants.ini"
     ])).map_err(|_| "Cannot intialise UCI engine")?;
