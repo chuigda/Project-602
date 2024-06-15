bca0e23a98949742314c86ab5c2adaabc3551c26
ICEYSELF <icey@icey.tech>
2024-06-15 12:26:04 +0800

    请用力抱紧我的身体 用吻填满所有的缝隙

--- src/fairy-stockfish/fairy-stockfish.ts
+++ src/fairy-stockfish/fairy-stockfish.ts
@@ -1,15 +1,17 @@
 export interface StockfishResource {
-   stockfishScript: string
    stockfishWasmBinary: ArrayBuffer
 }
 
 export async function loadStockfishResource(): Promise<StockfishResource> {
    setItemLoadProgress(0)
 
-   const script = await $().get('/fairy-stockfish/stockfish.js')
-   const scriptElement = document.createElement('script')
-   scriptElement.text = script
-   document.head.appendChild(scriptElement)
+   await new Promise(resolve => {
+      const scriptElement = document.createElement('script')
+      scriptElement.id = 'stockfish-script'
+      scriptElement.onload = resolve
+      scriptElement.src = '/fairy-stockfish/stockfish.js'
+      document.head.appendChild(scriptElement)
+   })
    setItemLoadProgress(0.02)
 
    const wasmBinary = await $().getWithProgressReport(
@@ -19,11 +21,35 @@ export async function loadStockfishResource(): Promise<StockfishResource> {
          setItemLoadProgress(0.02 + 0.98 * event.loaded / event.total)
       }
    ) as Blob
-   console.log('wasmBinary=', wasmBinary)
    setItemLoadProgress(1)
 
    return {
-      stockfishScript: script,
       stockfishWasmBinary: await wasmBinary.arrayBuffer()
    }
+}
+
+export class FairyStockfish {
+   instance: StockfishInstance
+   messageHandler: (line: string) => void = () => {}
+
+   constructor(instance: StockfishInstance) {
+      this.instance = instance
+   }
+}
+
+export async function createFairyStockfish(stockfishResource: StockfishResource): Promise<FairyStockfish> {
+   const instance = await Stockfish({ wasmBinary: stockfishResource.stockfishWasmBinary })
+
+   return new Promise(resolve => {
+      const fairyStockfish = new FairyStockfish(instance)
+      fairyStockfish.messageHandler = line => {
+         if (line.trim() == "uciok") {
+            fairyStockfish.messageHandler = () => {}
+            resolve(fairyStockfish)
+         }
+      }
+
+      instance.addMessageListener(line => fairyStockfish.messageHandler(line))
+      instance.postMessage("uci")
+   })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,42 @@
 import { loadChessboardAsset } from './chessboard/chessboard'
-import { loadStockfishResource } from './fairy-stockfish/fairy-stockfish'
+import { createFairyStockfish, loadStockfishResource } from './fairy-stockfish/fairy-stockfish'
 import { sleep } from './util/sleep'
 import { createCheckmateWindow } from './widgets/checkmate'
 
 async function continueLoadingOperation() {
-   // setItemLoadProgress(1)
-   await sleep(2000)
+   setItemLoadProgress(1)
+   await sleep(1000)
 
-   createCheckmateWindow({
-      startPos: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -',
-      movesPlayed: '1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 a6 6. Bg5 e6 7. f4 Qb6 8. Qd2 Qxb2 9. Rb1 Qa3 10. Rb3 Qa5 11. Be2 Nc6 12. Bxf6 gxf6 13. O-O Qc5 14. Kh1 Qxd4 15. Rd1 Qxd2 16. Rxd2 Be7 17. f5 Ne5 18. Na4 b5 19. Nb2 Bb7 20. fxe6 fxe6 21. Bh5+ Kf8 22. Re2 Rg8 23. c4 Rg5 24. Bf3 Bc6 25. cxb5 axb5 26. h4 Rg6 27. Nd3 Nxf3 28. gxf3 Rg3 29. Nf4 Kf7 30. Kh2 Rgg8 31. Rbe3 e5 32. Nd5 Bxd5 33. exd5 Ra4 34. Kh3 f5 0-1',
-      moveCount: 34,
+   // createCheckmateWindow({
+   //    startPos: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -',
+   //    movesPlayed: '1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 a6 6. Bg5 e6 7. f4 Qb6 8. Qd2 Qxb2 9. Rb1 Qa3 10. Rb3 Qa5 11. Be2 Nc6 12. Bxf6 gxf6 13. O-O Qc5 14. Kh1 Qxd4 15. Rd1 Qxd2 16. Rxd2 Be7 17. f5 Ne5 18. Na4 b5 19. Nb2 Bb7 20. fxe6 fxe6 21. Bh5+ Kf8 22. Re2 Rg8 23. c4 Rg5 24. Bf3 Bc6 25. cxb5 axb5 26. h4 Rg6 27. Nd3 Nxf3 28. gxf3 Rg3 29. Nf4 Kf7 30. Kh2 Rgg8 31. Rbe3 e5 32. Nd5 Bxd5 33. exd5 Ra4 34. Kh3 f5 0-1',
+   //    moveCount: 34,
 
-      brilliantCount: 0,
-      excellentCount: 1,
-      goodCount: 26,
-      interestingCount: 4,
-      inaccuracyCount: 0,
-      mistakeCount: 2,
-      blunderCount: 1,
-   })
+   //    brilliantCount: 0,
+   //    excellentCount: 1,
+   //    goodCount: 26,
+   //    interestingCount: 4,
+   //    inaccuracyCount: 0,
+   //    mistakeCount: 2,
+   //    blunderCount: 1,
+   // })
 
-   // $('load-item-title').innerText = '载入图形化操作界面'
-   // setOverallLoadProgress(1 / 3)
-   // await loadChessboardAsset()
+   $('load-item-title').innerText = '载入图形化操作界面'
+   setOverallLoadProgress(1 / 4)
+   await loadChessboardAsset()
 
-   // setOverallLoadProgress(2 / 3)
-   // $('load-item-title').innerText = '载入泛用型对抗人工智能'
-   // await loadStockfishResource()
+   setOverallLoadProgress(2 / 4)
+   $('load-item-title').innerText = '载入泛用型对抗人工智能'
+   const stockfishResource = await loadStockfishResource()
 
-   // setOverallLoadProgress(1)
-   // $('load-item-title').innerText = '即将完成...'
+   setOverallLoadProgress(3 / 4)
+   $('load-item-title').innerText = '人工智能系统初始化'
+   setItemLoadProgress(0)
+   const fairyStockfish = await createFairyStockfish(stockfishResource)
+   setItemLoadProgress(1)
+
+   setOverallLoadProgress(1)
+   $('load-item-title').innerText = '即将完成...'
 }
 
 continueLoadingOperation()
