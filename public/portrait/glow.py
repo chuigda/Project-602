2f88ae52f87d38a24fbb69f6d93569f1a9a921ee
ICEYSELF <icey@icey.tech>
2024-07-06 14:00:51 +0800

    add glow script and output images

--- public/portrait/glow.py
+++ public/portrait/glow.py
@@ -7,8 +7,8 @@ ThemeColors = {
 }
 
 def main(argv):
-   if len(argv) < 4:
-      print('Usage: python glow.py <image-path> <theme-color> <output-image>')
+   if len(argv) < 5:
+      print('Usage: python glow.py <image-path> <theme-color> <output-image> <conv-size>')
       return
 
    image_path = argv[1]
@@ -18,9 +18,10 @@ def main(argv):
    glow = image.copy()
    glow[glow[:, :, 3] != 0] = ThemeColors[argv[2]]
 
+   conv_size = int(argv[4])
    # create a blurred image
-   blurred = cv2.blur(glow, (31, 31))
-   # blurred = cv2.GaussianBlur(glow, (63, 63), 0)
+   blurred = cv2.blur(glow, (conv_size, conv_size))
+   # blurred = cv2.GaussianBlur(glow, (conv_size, conv_size), 0)
 
    # put the original image on top of the blurred image
    result = blurred.copy()
@@ -28,8 +29,8 @@ def main(argv):
 
    cv2.imwrite(argv[3], result)
 
-   # cv2.imshow('result', cv2.resize(result, (image.shape[1] // 2, image.shape[0] // 2)))
-   # cv2.waitKey(0)
+   cv2.imshow('result', cv2.resize(result, (image.shape[1] // 2, image.shape[0] // 2)))
+   cv2.waitKey(0)
 
 if __name__ == '__main__':
     main(sys.argv)
---
+++ public/portrait/glow.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+python glow.py black_king.png red black_king_glow.png 63
+python glow.py black_queen_base.png red black_queen_base_glow.png 63
+python glow.py black_bishop.png red black_bishop_glow.png 127
+
+python glow.py white_king.png cyan white_king_glow.png 63
+python glow.py white_bishop.png cyan white_bishop_glow.png 127
