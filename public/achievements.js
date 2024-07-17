45d555429cbb31a47e0a2cf19bf29eb148569f67
ICEYSELF <icey@icey.tech>
2024-07-18 03:20:41 +0800

    进行一些文本工作

---
+++ public/achievements.js
@@ -0,0 +1,107 @@
+[
+   // 战斗
+   {
+      id: 'first_blood',
+      name: '第一滴血',
+      desc: '捕获第一颗棋子'
+   },
+
+   // 开局
+   {
+      id: 'pawn_lives_matter',
+      name: '兵命贵！',
+      desc: '使用一个弃兵开局'
+   },
+   {
+      id: 'funny_horse_jump',
+      name: '马小跳',
+      desc: '开局第一步跳马'
+   },
+   {
+      id: 'super_waiting_move',
+      name: '闲敲棋子落灯花',
+      desc: '开局第一步走 a3 或者 a6'
+   },
+   {
+      id: 'rook_lift',
+      name: '铁滑车？',
+      desc: '开局第一步走 a4, h4, a5 或者 h5'
+   },
+   {
+      id: 'tea_moment',
+      name: '下午茶时间',
+      desc: '使用英国式开局 (1. c4)'
+   },
+   {
+      id: 'ke2_brilliant',
+      name: '御驾亲征',
+      desc: '开局三步以内，在没有被将军的情况下动王'
+   },
+   {
+      id: 'icbm',
+      name: '洲际导弹',
+      desc: '成功实施洲际导弹弃兵（坦尼森弃兵或布达佩斯弃兵）'
+   },
+   {
+      id: 'suicide',
+      name: '没逝找逝',
+      desc: '开局走 1. f3 2. g4 或者 1...f6 2...g5'
+   },
+
+   // 升变
+   {
+      id: 'queen_promote',
+      name: 'Queen! 稳啦，稳啦！',
+      desc: '升变一个皇后'
+   },
+   {
+      id: 'knight_promote',
+      name: '骑兵连，冲锋！',
+      desc: '升变一个骑士'
+   },
+   {
+      id: 'rook_promote',
+      name: '逐一发送坦克',
+      desc: '升变一个城堡'
+   },
+   {
+      id: 'bishop_promote',
+      name: '教会分裂',
+      desc: '升变一个主教'
+   },
+
+   {
+      id: 'rook_fleet',
+      name: '钢铁洪流',
+      desc: '场上同时存在三个或者更多己方城堡'
+   },
+   {
+      id: 'harem',
+      name: '后宫佳丽三千人',
+      desc: '场上同时存在两个或者更多己方皇后'
+   },
+
+   // 将杀与和棋
+   {
+      id: 'chinese_juhe',
+      name: '中式居合',
+      desc: '用城堡将死对手'
+   },
+   {
+      id: 'knight_mate',
+      name: '当阳常志此心丹',
+      desc: '用骑士将死对手'
+   },
+   {
+      id: 'thou shalt not pass',
+      name: 'Thou shalt not pass',
+      desc: '逼和对手'
+   },
+
+   // 残局
+   {
+      id: 'obiwan',
+      name: '结束了安纳金！',
+      desc: '在兵残局中用王率先占据高地并取胜'
+   }
+]
