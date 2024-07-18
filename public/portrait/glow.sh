89b8f34dc3a1fe0782cc5895aaee90b598dba2d6
ICEYSELF <icey@icey.tech>
2024-07-18 18:39:02 +0800

    add glowed items

--- public/portrait/glow.sh
+++ public/portrait/glow.sh
@@ -5,4 +5,6 @@ python glow.py black_queen_base.png red black_queen_base_glow.png 63
 python glow.py black_bishop.png red black_bishop_glow.png 127
 
 python glow.py white_king.png cyan white_king_glow.png 63
+python glow.py white_queen.png cyan white_queen_glow.png 63
+python glow.py white_queen_fan.png cyan white_queen_fan_glow.png 63
 python glow.py white_bishop.png cyan white_bishop_glow.png 127
